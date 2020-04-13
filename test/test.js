c8fb3200ca98fbc941ec969e18f71e10f43e2253
diegoflores4711 <diegoflores4711@hotmail.com>
2020-04-13 20:47:10 +0200

    added travis.yml

---
+++ test/test.js
@@ -0,0 +1,12 @@
+//import { num } from './math.js';
+
+function num(a) {
+    if(a == 2)
+        return true;
+    else
+        return false;
+}
+
+QUnit.test( "hello test", function( assert ) {
+    assert.ok( num(1), "Passed!" );
+  });
