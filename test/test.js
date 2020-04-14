25701fdd2b069d31988cc82e4a35b151e70a3438
diegoflores4711 <diegoflores4711@hotmail.com>
2020-04-14 17:58:56 +0200

    testing

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 //var m = require("./math.js");
 //require("./math.js");
 
-const modu = require('./math.js');
+const modu = require('./math');
 
 QUnit.test( "hello test", function( assert ) {
     assert.ok( modu.num(2), "Passed!" );
