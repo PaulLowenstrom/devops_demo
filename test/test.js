d7617a17145fb9e90555070efff7e287fcb61cf9
diegoflores4711 <diegoflores4711@hotmail.com>
2020-04-14 17:25:34 +0200

    testing

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-//import { num } from './math.js';
+import { num } from './math.js';
 //var m = require("./math.js");
-require("./math.js");
+//require("./math.js");
 
 QUnit.test( "hello test", function( assert ) {
     assert.ok( num(2), "Passed!" );
