368b3723de0244fda46dc33fbd9f789a5be3b608
Nico <nicolaih@kth.se>
2020-04-22 15:03:41 +0200

    added easter egg

--- math.js
+++ math.js
@@ -1,8 +1,15 @@
 function div_math(a = document.getElementById("math_input_a").value, b = document.getElementById("math_input_b").value) {
     let div = a / b;
     document.getElementById("math_ans").innerHTML = "Answer: " + div;
-}
+    if (b == 0) {
+        document.getElementById("end_screen").src = "win.jpg";
+        document.getElementById("end_screen").style.visibility = "visible";
 
+    }
+    else {
+        document.getElementById("end_screen").style.visibility = "hidden";
+    }
+}
 // for testing
 module.exports = {
     div: function div_math(a = document.getElementById("math_input_a").value, b = document.getElementById("math_input_b").value) {
