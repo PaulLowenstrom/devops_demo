f08f23226b81733c0c53736ccf2d5fbabcdb67d5
diegoflores4711 <diegoflores4711@hotmail.com>
2020-04-22 13:20:16 +0200

    refactor travis.yml

---
+++ scripts/gcloud.sh
@@ -0,0 +1,10 @@
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then 
+  rm -rf $HOME/google-cloud-sdk; export
+  CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; 
+fi 
+source /home/travis/google-cloud-sdk/path.bash.inc
+
+gcloud components install kubectl
+gcloud auth activate-service-account $gcloud_service_Acc --key-file key.json --project $gcloud_project
+gcloud container clusters get-credentials $gcloud_cluster --zone $gcloud_zone --project $gcloud_project
+kubectl set image deployment/demo nodejs=$image:$TRAVIS_COMMIT --record=true
